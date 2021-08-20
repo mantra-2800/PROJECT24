e16a6b2c2224b3e5d82122e16cf021a1a3e8e774
mantra-2800 <83644198+mantra-2800@users.noreply.github.com>
2021-08-20 19:09:39 +0530

    Add files via upload

---
+++ PARCEL.js
@@ -0,0 +1,21 @@
+/*class Parcel{
+    constructor(x,y,w,h){
+        var options = {
+            'restitution':0.4,
+            'isStatic' : false,
+            'gravitation':0.3
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.w=w;
+        this.h=h;
+
+        World.add(world,this.body);
+    }
+    display(){
+        var pos =this.body.positon;
+        var angle =this.body.angle;
+
+
+        
+    }
+};*/
